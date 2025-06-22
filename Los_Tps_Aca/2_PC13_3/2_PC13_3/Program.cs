18b5d168c844a82dcd32dae498d4077bf863db5c
VAdnoif <valen.fionda@gmail.com>
2025-06-22 15:54:36 -0300

    Add files via upload

---
+++ Los_Tps_Aca/2_PC13_3/2_PC13_3/Program.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_PC13_3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine("Ingrese la cantidad de participantes en el torneo");
+            int cantPart = int.Parse(Console.ReadLine());
+
+            int[] participantes = new int[cantPart];
+
+            Console.WriteLine("Ingrese los puntajes de los participantes ");
+
+            for (int i = 0; i < participantes.Length; i++)
+            {
+                Console.WriteLine("Ingrese el puntaje del participante {0}: ",i+1);
+                participantes[i] = int.Parse(Console.ReadLine());
+            }
+
+            for(int i = 0; i < participantes.Length; i++)
+            {
+                for (int j = 0; j < participantes.Length - 1; j++)
+                {
+                    if(participantes[j] < participantes[j + 1])
+                    {
+                        int temp = participantes[j];
+                        participantes[j] = participantes[j + 1];
+                        participantes[j + 1] = temp;
+                    }
+                }
+            }
+
+
+            Console.WriteLine();
+
+            Console.WriteLine("Los puntajes de mayor a menor son: ");
+
+            for (int i = 0; i < participantes.Length; i++)
+            {
+                
+                Console.WriteLine("el puntaje numero {0} es: {1} ",i+1, participantes[i]);
+
+            }
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ Los_Tps_Aca/2_PC13_4/2_PC13_4/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2_PC13_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ingrese la cantidad de productos vendidos: ");
+            int cantProduc = int.Parse(Console.ReadLine());
+
+            int[] productos = new int[cantProduc];
+
+            for(int i = 0; i < productos.Length; i++)
+            {
+                Console.WriteLine("Ingrese el valor del producto numero {0}: ", i + 1);
+                productos[i] = int.Parse(Console.ReadLine());
+            }
+
+            for(int i = 0; i < productos.Length; i++)
+            {
+                for(int j = 0; j < productos.Length-1; j++)
+                {
+                    if(productos[j] < productos[j+1])
+                    {
+                        int temp = productos[j];
+                        productos[j] = productos[j+1];
+                        productos[j+1] = temp;
+                    }
+                }
+            }
+
+            for(int i = 0; i < productos.Length; i++)
+            {
+                Console.WriteLine("El valor del producto {0} ordenado es: {1}",i+1,productos[i]);
+            }
+
+
+            Console.ReadKey();
+        }
+    }
+}
